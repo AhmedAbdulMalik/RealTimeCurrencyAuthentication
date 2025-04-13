b5a6e8a1ca5e7d2b691b2772063f3081ad01058c
AhmedAbdulMalik <abdulmalikandanas@gmail.com>
2025-04-13 19:37:16 +0530

    achieved the black screen

---
+++ main.js
@@ -0,0 +1,19 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1,1000);
+//camera(field_of_view, aspect_ratio, view_frustum)
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,window.innerHeight);
+camera.position.setZ(30);
+//renderer==DRAW
+renderer.render(scene,camera);
+
